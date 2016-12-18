321049418ef7d28d9b06592f74c231140d06b777
kasora <kasorasun@gmail.com>
2016-12-19 02:51:27 +0800

    new feature add the config.py to help you set params quickly. by using PyLint, my code looks pretty now, right :)

---
+++ config.sample.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' write your data here and delete the "sample" in filename. '
+
+DATABASE = {
+    'user' : "",
+    'password' : "",
+    'name' :  ""
+}
+BILIBILI = {
+    'usernumber' : 70000000,
+    'interval' : 1000
+}
--- master.py
+++ master.py
@@ -1,72 +1,87 @@
 #!/usr/bin/env python3
 # -*- coding : utf-8 -*-
 
-import time,queue
+' It will be set in server. '
+
 from multiprocessing.managers import BaseManager
 from multiprocessing import freeze_support
+import time
+import queue
 import pymysql
+import config
 
-user = "";
-passwd = "";
-db =  "";
-userNumber = 70000000;
+USER = config.DATABASE['user']
+PASSWD = config.DATABASE['password']
+DBNAME = config.DATABASE['name']
+
+SERVER_PORT = config.MASTER['server_port']
+AUTHKEY = config.MASTER['authkey']
+
+USER_NUMBER = config.BILIBILI['usernumber']
+INTERVAL = config.BILIBILI['interval']
+TASK_NUMBER = USER_NUMBER//INTERVAL
+
+_task_queue = queue.Queue(TASK_NUMBER)
+_result_queue = queue.Queue(TASK_NUMBER)
+
+def _gettask():
+    return _task_queue
+
+def _getresult():
+    return _result_queue
+
+def startserver():
+
+    """start the server"""
+
+    BaseManager.register('get_task', callable=_gettask)
+    BaseManager.register('get_result', callable=_getresult)
+    manager = BaseManager(address=('0.0.0.0', SERVER_PORT), authkey=AUTHKEY)
+
+    manager.start()
+
+    task = manager.get_task()
+    result = manager.get_result()
+    lastset = set(range(TASK_NUMBER))
 
 
-jump = 1000;
+    for i in range(TASK_NUMBER):
+        task.put(i)
 
-task_number = userNumber//jump;
-
-task_queue = queue.Queue(task_number);
-result_queue = queue.Queue(task_number);
-
-lastSet = set(range(task_number));
-
-def gettask():
-    return task_queue;
-def getresult():
-     return result_queue;
- 
-def test():
-    BaseManager.register('get_task',callable = gettask);
-    BaseManager.register('get_result',callable = getresult);
-    manager = BaseManager(address = ('0.0.0.0',5011),authkey = b'kasora');
-
-    manager.start();
-
-    task = manager.get_task();
-    result = manager.get_result();
-
-
-    for i in range(task_number):
-        task.put(i);
-
-    while(len(lastSet)>0):
-        time.sleep(60);
-        print(str(len(lastSet))+" left")
-        conn = pymysql.connect(host="127.0.0.1",user=user,passwd=passwd,db=db,use_unicode=True, charset="utf8")
+    while len(lastset) > 0:
+        time.sleep(60)
+        print(str(len(lastset))+" left")
+        conn = pymysql.connect(
+            host="127.0.0.1",
+            user=USER,
+            passwd=PASSWD,
+            db=DBNAME,
+            use_unicode=True,
+            charset="utf8"
+        )
         cur = conn.cursor()
         for i in range(result.qsize()):
-            ans = result.get();
+            ans = result.get()
             try:
-                lastSet.remove(int(ans[0]));
+                lastset.remove(int(ans[0]))
             except KeyError:
-                continue;
-            if(len(ans[1])==0):
-                pass;
+                continue
+            if len(ans[1]) == 0:
+                pass
             else:
                 for userinfo in ans[1]:
-                    insertsql = 'insert into vipuserinfo (uid, ulevel) values (%s, %s)';
-                    cur.execute(insertsql, [userinfo["uid"], userinfo["level"]]);
-                    conn.commit();
+                    insertsql = 'insert into vipuserinfo (uid, ulevel) values (%s, %s)'
+                    cur.execute(insertsql, [userinfo["uid"], userinfo["level"]])
+                    conn.commit()
         cur.close()
         conn.close()
-        if(task.empty() and len(lastSet)>0):
-            for i in lastSet:
-                task.put(i);
-    time.sleep(1);
+        if task.empty() and len(lastset) > 0:
+            for i in lastset:
+                task.put(i)
+    time.sleep(1)
 
-    manager.shutdown();
-        
+    manager.shutdown()
+
 if __name__ == '__main__':
     freeze_support()
-    test();
+    startserver()
